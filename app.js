58ff3b2a0e4a58329d303f70436dee8f2e0778b7
Emanuel Astudillo <astudillo.emanuel@gmail.com>
2025-03-13 22:04:40 -0300

    arreglos de textos

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 // Inicializar Flatpickr en el input
 flatpickr("#birthday", {
-  dateFormat: "d/m/Y", // Formato de fecha
-  maxDate: "today", // No permitir fechas futuras
-  locale: "es", // Idioma español
-  defaultDate: "01/01/2000", // Fecha por defecto
-  onChange: function (selectedDates, dateStr, instance) {
-    console.log("Fecha seleccionada:", dateStr);
-  },
+    dateFormat: "d/m/Y", // Formato de fecha
+    maxDate: "today", // No permitir fechas futuras
+    locale: "es", // Idioma español
+    defaultDate: "01/01/2000", // Fecha por defecto
+    onChange: function (selectedDates, dateStr, instance) {
+        console.log("Fecha seleccionada:", dateStr);
+    },
 });
 
 // Manejar el envío del formulario
 document
-  .getElementById("birthdayForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    const selectedDate = document.getElementById("birthday").value;
+    .getElementById("birthdayForm")
+    .addEventListener("submit", function (event) {
+        event.preventDefault();
+        const selectedDate = document.getElementById("birthday").value;
 
-    // Usar Luxon para calcular la edad
-    const DateTime = luxon.DateTime;
-    const birthDate = DateTime.fromFormat(selectedDate, "dd/MM/yyyy"); // Parsear la fecha
-    const now = DateTime.now(); // Fecha actual
-    const diff = now.diff(birthDate, ["years", "months"]); // Calcular la diferencia en años y meses
+        // Usar Luxon para calcular la edad
+        const DateTime = luxon.DateTime;
+        const birthDate = DateTime.fromFormat(selectedDate, "dd/MM/yyyy"); // Parsear la fecha
+        const now = DateTime.now(); // Fecha actual
+        const diff = now.diff(birthDate, ["years", "months", "days"]).toObject(); // Calcular la diferencia en años y meses
 
-    // Mostrar la edad
-    document.getElementById("ageResult").innerText = `Tienes ${Math.floor(
-      diff.years
-    )} años y ${Math.floor(diff.months)} ${
-      Math.floor(diff.months) === 1 ? "mes" : "meses"
-    } de edad`;
-  });
+        // Mostrar la edad
+        document.getElementById("ageResult").innerText = `Tienes ${Math.floor(
+            diff.years
+        )} años, ${Math.floor(diff.months)} ${Math.floor(diff.months) === 1 ? "mes" : "meses"
+            } y ${Math.floor(diff.days)} ${Math.floor(diff.days) === 1 ? "dia" : "dias"
+            } de edad`;
+    });
